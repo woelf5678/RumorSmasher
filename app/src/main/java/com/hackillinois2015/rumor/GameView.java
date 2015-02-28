539c8090968c43d87d1d4d21d2e1bc438117dea7
woelf5678 <davidguo486@gmail.com>
2015-02-28 11:06:46 -0600

    Transfer files

--- app/src/main/java/com/hackillinois2015/rumor/GameView.java
+++ app/src/main/java/com/hackillinois2015/rumor/GameView.java
@@ -19,4 +19,36 @@ public class GameView extends View {
     public GameView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
+
+    final static double d60_DEGREES = Math.PI/3;
+    final static double dHexagonSide = 1;
+    final static double dHexagonUseful = dHexagonSide*Math.sqrt(3);
+
+    static <T> void print(T[] arr)
+    {
+        for(T x: arr)
+        {
+            System.out.print(x);
+            System.out.print("  ");
+        }
+        System.out.print(System.lineSeparator());
+    }
+
+    static Integer[] xyToHexagon(int x,int y)
+    {
+        double u=x-y/Math.tan(d60_DEGREES);
+        double v=y/Math.sin(d60_DEGREES);
+        return new Integer[]{(int)Math.floor(u/dHexagonUseful), (int)Math.floor(v/dHexagonUseful)};
+    }
+
+    static int floorDiv(int a, int b)
+    {
+        if(a%b==0)
+            return a/b;
+        boolean posA = a>0;
+        boolean posB = b>0;
+        if(posA==posB)
+            return a/b;
+        else return a/b-1;
+    }
 }
