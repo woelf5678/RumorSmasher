4bb366aba7b7d1083598e7f88462be149b3c7e5e
C4Phone <qq2900726136@gmail.com>
2015-02-28 11:05:24 -0600

    transfer files.

---
+++ app/src/main/java/com/hackillinois2015/rumor/GameView.java
@@ -0,0 +1,22 @@
+package com.hackillinois2015.rumor;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Mark on 2/28/2015.
+ */
+public class GameView extends View {
+    public GameView(Context context) {
+        super(context);
+    }
+
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public GameView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
